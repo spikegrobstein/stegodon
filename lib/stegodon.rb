15d57a33a0aad13e13091989ae87bd8ebc758342
Spike Grobstein <spike@ticketevolution.com>
2012-11-14 16:54:03 -0500

    include all the necessary libs in the main one

--- lib/stegodon.rb
+++ lib/stegodon.rb
@@ -1,5 +1,8 @@
 require "stegodon/version"
-
+require 'stegodon/dsl'
+require 'stegodon/base'
+require 'stegodon/configuration'
+require 'stegodon/backup'
 
 module Stegodon
   # Your code goes here...
